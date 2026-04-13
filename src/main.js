aa06b05c12d996e8ebf51bdd3689d3fb289d98dc
Osman Gözütok <osmangztk16@gmail.com>
2026-04-13 11:19:23 +0300

    aos eklendi navbar çözüldü

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
 /* main.js */
 import "flowbite";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration: 800,
+  once: true,
+  offset: 100,
+  easing: "ease-in-out",
+});
+document.addEventListener("click", (e) => {
+  const menu = document.getElementById("navbar-cta");
+  const button = document.querySelector("[data-collapse-toggle='navbar-cta']");
+
+  const isOpen = !menu.classList.contains("hidden");
+
+  if (!isOpen) return;
+
+  // linke tıklandıysa veya dışarı tıklandıysa kapat
+  if (
+    e.target.closest("#navbar-cta a") ||
+    (!e.target.closest("#navbar-cta") &&
+      !e.target.closest("[data-collapse-toggle]"))
+  ) {
+    button.click();
+  }
+});
